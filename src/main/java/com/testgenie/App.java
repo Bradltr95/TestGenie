f95c22287dfc0cda406bdb38ac3969fc490e7190
Bradley Latreille <bradltr95@gmail.com>
2025-07-27 00:17:05 -0400

    feat: implement `Callable<Integer>` interface to support picocli execution.
    - Implements the interface for picoli and overrides the interfaces call method.The implementation is still required.

--- src/main/java/com/testgenie/App.java
+++ src/main/java/com/testgenie/App.java
@@ -2,14 +2,21 @@ package com.testgenie;
 
 import picocli.CommandLine;
 
+import java.util.concurrent.Callable;
+
 @CommandLine.Command(
         name = "testgenie",
         description = "Generates JUnit 5 test classes from Java source files.",
         mixinStandardHelpOptions = true
 )
-public class App {
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
         // Standard outputs should not be used to directly log anything. This is just Stub code for now.
         System.out.println("Test Genie App");
+    }
+
+    @Override
+    public Integer call() {
+        return 0;
     }
 }
