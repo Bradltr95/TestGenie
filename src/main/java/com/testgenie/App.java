a9c07256986006d4c11a8189b951fbb62c79185b
Bradley Latreille <bradltr95@gmail.com>
2025-07-27 00:13:05 -0400

    feat: add `@Command` annotation for CLI definition

--- src/main/java/com/testgenie/App.java
+++ src/main/java/com/testgenie/App.java
@@ -1,5 +1,12 @@
 package com.testgenie;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "testgenie",
+        description = "Generates JUnit 5 test classes from Java source files.",
+        mixinStandardHelpOptions = true
+)
 public class App {
     public static void main(String[] args) {
         // Standard outputs should not be used to directly log anything. This is just Stub code for now.
